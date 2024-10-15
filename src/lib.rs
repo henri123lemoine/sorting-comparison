a00d24fec985c31a5b4494c8f40cf5ade1a2f7ce
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-15 14:48:43 -0400

    Implemented bogo sort in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
 use pyo3::prelude::*;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn is_sorted(arr: Vec<i32>) -> bool {
+    arr.windows(2).all(|w| w[0] <= w[1])
+}
+
+#[pyfunction]
+fn bogo_sort(mut arr: Vec<i32>) -> Vec<i32> {
+    let mut rng = thread_rng();
+    while !is_sorted(arr.clone()) {
+        arr.shuffle(&mut rng);
+    }
+    arr
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn sorting_comparison(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(bogo_sort, m)?)?;
     Ok(())
 }
