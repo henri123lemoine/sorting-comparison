47b5fdb725a19eda1f26c21b584b8e4408c784a1
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-15 14:29:00 -0400

    Added bogo-sort implementation

---
+++ rust/src/lib.rs
@@ -0,0 +1,23 @@
+use pyo3::prelude::*;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+#[pyfunction]
+fn is_sorted(arr: &[i32]) -> bool {
+    arr.windows(2).all(|w| w[0] <= w[1])
+}
+
+#[pyfunction]
+fn bogo_sort(mut arr: Vec<i32>) -> Vec<i32> {
+    let mut rng = thread_rng();
+    while !is_sorted(&arr) {
+        arr.shuffle(&mut rng);
+    }
+    arr
+}
+
+#[pymodule]
+fn rust_bogo_sort(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(bogo_sort, m)?)?;
+    Ok(())
+}
