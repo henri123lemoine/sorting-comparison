f0379191eac70374cf7b3334cf2ed7e4a6d1ac07
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-15 16:41:40 -0400

    Removed unused main() in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use rand::seq::SliceRandom;
-use std::io::{self, BufRead};
 
 pub fn is_sorted(arr: &[i32]) -> bool {
     arr.windows(2).all(|w| w[0] <= w[1])
@@ -10,22 +9,4 @@ pub fn bogo_sort(arr: &mut [i32]) {
     while !is_sorted(arr) {
         arr.shuffle(&mut rng);
     }
-}
-
-fn main() {
-    let stdin = io::stdin();
-    let mut line = String::new();
-    stdin.lock().read_line(&mut line).unwrap();
-
-    let mut numbers: Vec<i32> = line
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    bogo_sort(&mut numbers);
-
-    for num in numbers {
-        print!("{} ", num);
-    }
-    println!();
 }
