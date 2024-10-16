d1629e5971582cea7612b2d73f2975d53b470bef
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-15 21:31:25 -0400

    Nice plots

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use std::io;
 
 pub fn is_sorted(arr: &[i32]) -> bool {
     arr.windows(2).all(|w| w[0] <= w[1])
@@ -9,4 +10,21 @@ pub fn bogo_sort(arr: &mut [i32]) {
     while !is_sorted(arr) {
         arr.shuffle(&mut rng);
     }
+}
+
+#[warn(dead_code)]
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut numbers: Vec<i32> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    bogo_sort(&mut numbers);
+
+    for num in numbers {
+        print!("{} ", num);
+    }
+    println!();
 }
